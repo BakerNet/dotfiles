80a300d8d37303a8eab00b2602f6e83954d12397
BakerNet <hansbaker90@gmail.com>
2025-02-06 15:10:42 -0800

    update nvim git shortcuts

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -23,12 +23,8 @@ return {
 
             vim.keymap.set('n', '[g', function() require('gitsigns').nav_hunk('prev') end,
                 { buffer = bufnr, desc = 'Previous [G]it Hunk' })
-            vim.keymap.set('n', '<leader>gp', function() require('gitsigns').nav_hunk('prev') end,
-                { buffer = bufnr, desc = '[G]it Hunk [P]revious' })
             vim.keymap.set('n', ']g', function() require('gitsigns').nav_hunk('next') end,
                 { buffer = bufnr, desc = 'Next [G]it Hunk' })
-            vim.keymap.set('n', '<leader>gn', function() require('gitsigns').nav_hunk('next') end,
-                { buffer = bufnr, desc = '[G]it Hunk [N]ext' })
             vim.keymap.set('n', '<leader>gh', require('gitsigns').preview_hunk,
                 { buffer = bufnr, desc = 'Previw [G]it [H]unk' })
             vim.keymap.set('n', '<leader>gb', require('gitsigns').toggle_current_line_blame,
@@ -40,9 +36,16 @@ return {
             vim.keymap.set('n', '<leader>gr', require('gitsigns').undo_stage_hunk,
                 { buffer = bufnr, desc = '[G]it [R]eset (Undo previous) Hunk' })
             vim.keymap.set('n', '<leader>gs', '<Cmd>Git status<CR>', { desc = '[G]it [S]tatus' })
+            vim.keymap.set('n', '<leader>gM', '<Cmd>Git checkout origin/master<CR>', { desc = '[G]it checkout [M]aster' })
+            vim.keymap.set('n', '<leader>gSS', '<Cmd>Git stash<CR>', { desc = '[G]it [S]tash' })
+            vim.keymap.set('n', '<leader>gSP', '<Cmd>Git stash pop<CR>', { desc = '[G]it [S]tash [P]op' })
             vim.keymap.set('n', '<leader>gaa', '<Cmd>Git add .<CR>', { desc = '[G]it [A]dd All' })
-            vim.keymap.set('n', '<leader>gP', '<Cmd>Git push<CR>', { desc = '[G]it [P]ush' })
-            vim.keymap.set('n', '<leader>gF', '<Cmd>Git fetch<CR>', { desc = '[G]it [F]etch' })
+            vim.keymap.set('n', '<leader>gpp', '<Cmd>Git push<CR>', { desc = '[G]it [P]ush' })
+            vim.keymap.set('n', '<leader>gpf', '<Cmd>Git push --force-with-lease<CR>',
+                { desc = '[G]it [P]ush [F]orce' })
+            vim.keymap.set('n', '<leader>gP', '<Cmd>Git pull<CR>', { desc = '[G]it [P]ull' })
+            vim.keymap.set('n', '<leader>gf', '<Cmd>Git fetch<CR>', { desc = '[G]it [F]etch' })
+            vim.keymap.set('n', '<leader>gB', ':Git switch -c ', { desc = '[G]it Create [B]ranch' })
         end
     },
     {
